957d3efe3b447423dff9040a46e0002f77f18969
Martin Hjord <hjorden@gmail.com>
2016-10-03 11:20:06 +0200

    Bla

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AarhusWebDevCoop.ViewModels;
+using Umbraco.Web.Mvc;
+using System.Net.Mail;
+using Umbraco.Core.Models;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurfaceController
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm form)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            MailMessage message = new MailMessage();
+            message.To.Add("mymail@gmail.com");
+            message.Subject = form.Subject;
+            message.From = new MailAddress(form.Email, form.Name);
+            message.Body = form.Message + "\n my email is: " + form.Email;
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.Credentials = new NetworkCredential("mymail@gmail.com", "mypassword");
+                smtp.EnableSsl = true;
+
+                smtp.Send(message);
+            }
+
+            TempData["success"] = true;
+
+            IContent comment = Services.ContentService.CreateContent(form.Subject, CurrentPage.Id, "Comment");
+
+            comment.SetValue("commentName", form.Name);
+            comment.SetValue("subject", form.Subject);
+            comment.SetValue("email", form.Email);
+            comment.SetValue("message", form.Message);
+
+            //Save
+            Services.ContentService.Save(comment);
+
+
+            //Save & publish
+            //Services.ContentService.SaveAndPublishWithStatus(comment);
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ AarhusWebDevCoop/Controllers/MessageBoardFormSurfaceController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AarhusWebDevCoop.ViewModels;
+using Umbraco.Core.Models;
+using Umbraco.Web.Mvc;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class MessageBoardFormSurfaceController : SurfaceController
+    {
+        // GET: MessageBoardFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("MessageBoardForm", new Message());
+        }
+
+        [HttpPost]
+        public ActionResult HandleSubmit(Message model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            IContent message = Services.ContentService.CreateContent(model.Name, CurrentPage.Id, "Message");
+            message.SetValue("messageName", model.Name);
+            message.SetValue("messageBody", model.Msg);
+
+            Services.ContentService.SaveAndPublishWithStatus(message);
+
+
+
+
+            return RedirectToCurrentUmbracoPage();
+        }
+
+
+    }
+}
